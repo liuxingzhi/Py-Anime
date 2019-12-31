45e04e96bf2f088fa12ff7647253af33ba2fd9c1
xingzhi <xingzhi2@illinois.edu>
2019-12-31 17:27:37 +0800

    new commit

--- farewell.py
+++ farewell.py
@@ -3,9 +3,28 @@ import random
 
 SCREEN_HEIGHT = 900
 SCREEN_WIDTH = 1500
-SCREEN_SIZE = (SCREEN_HEIGHT, SCREEN_WIDTH)
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
 
+pygame.init()
+screen = pygame.display.set_mode(SCREEN_SIZE)
+pygame.display.set_caption("18年")
+
+# TODO add background for screen
 
 
 if __name__ == "__main__":
-    print("farewell")
+    clock = pygame.time.Clock()
+    done = False
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        # TODO show background
+        # screen.blit(bg, (0, 0))
+
+        # TODO show texts
+
+        # update the contents of the entire display
+        pygame.display.flip()
+        clock.tick(40)
